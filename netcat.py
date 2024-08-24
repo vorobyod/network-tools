8e427cf8a2ebc3bc9f96377cacfe825a5b33f963
Denys Vorobyov <vorobyoff.denys@gmail.com>
2024-08-24 21:54:17 +0300

    Added listener and handler methods

--- netcat.py
+++ netcat.py
@@ -11,7 +11,7 @@ def execute(cmd):
     cmd = cmd.strip()
     if not cmd:
         return
-    output = subprocess.check_output(shlex.split(cmd)),
+    output = subprocess.check_output(shlex.split(cmd),
                                      stderr=subprocess.STDOUT)
     return output.decode()
 
@@ -49,12 +49,19 @@ class NetCat:
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
+    #
+    # We either run in terminal mode or
+    # listening and waiting for commands to run (-lceu)
+    #
     def run(self):
         if self.args.listen:
             self.listen()
         else:
             self.send()
 
+    #
+    # Communicate with the network target
+    #
     def send(self):
         self.socket.connect((self.args.target, self.args.port))
         if self.buffer:
@@ -82,6 +89,55 @@ class NetCat:
             self.socket.close()
             sys.exit()
 
-            
+    #
+    # Accept client connection and run the handler thread
+    #
+    def listen(self):
+        self.socket.bind((self.args.target, self.args.port))
+        self.socket.listen(5)
+        while True:
+            client_socket, _ = self.socket.accept()
+            client_thread = threading.Thread(
+                target=self.handle, args=(client_socket,)
+            )
+            client_thread.start()
 
+    #
+    # Request handler
+    #
+    def handle(self, client_socket):
+        if self.args.execute:
+            output = execute(self.args.execute)
+            client_socket.send(output.encode())
+
+        elif self.args.upload:
+            file_buffer = b''
+            while True:
+                data = client_socket.recv(4096)
+                if data:
+                    file_buffer += data
+                else:
+                    break
+
+            with open(self.args.upload, 'wb') as f:
+                f.write(file_buffer)
+
+            message = f'Saved file {self.args.upload}'
+            client_socket.send(message.encode())
+
+        elif self.args.command:
+            cmd_buffer = b''
+            while True:
+                try:
+                    client_socket.send(b'#> ')
+                    while '\n' not in cmd_buffer.decode():
+                        cmd_buffer += client_socket.recv(64)
+                    response = execute(cmd_buffer.decode())
+                    if response:
+                        client_socket.send(response.encode())
+                    cmd_buffer = b''
+                except Exception as e:
+                    print(f'server killed {e}')
+                    self.socket.close()
+                    sys.exit()
 
